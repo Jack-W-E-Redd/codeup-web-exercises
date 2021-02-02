12815d3d00b08f25fcf98409d84c8d42cd41762a
jack redd <jack.w.e.redd@gmail.com>
2021-02-02 17:12:25 -0600

    Add JQuery exercises

--- js/JQuery-custom-exercise.js
+++ js/JQuery-custom-exercise.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-    alert("Dom is ready to go!")
-})
+// $(document).ready(function() {
+//     alert("Dom is ready to go!")
+// })
+"use strict";
+
+
+$(document).ready(function(){
+    $('li').css('font-size', '20px');
+    $('h1, p, li').css("background", "green")
+    let headingAlert = $('h1').html();
+    alert(headingAlert);
+});
