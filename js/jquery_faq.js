56a094cc71bf0aca04ac1e9a9e984f00014d1e0a
jack redd <jack.w.e.redd@gmail.com>
2021-02-05 08:56:00 -0600

    Jquery FAQ exercise part 2 fully functional

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,2 +1,24 @@
 "use strict";
 
+// $(document).ready(function(){
+
+
+
+$(document).click(function(e){
+        e.preventDefault();
+        $('.invisible').toggleClass();
+        $('dt').toggleClass('highlighted');
+
+    })
+    $('.button-background').click(function (e){
+        e.preventDefault();
+        $('ul li:last-child').css('background', 'yellow');
+    });
+    $('h3').click(function (e){
+        e.preventDefault();
+        $('ul').children().css('font-weight', 'bold');
+    });
+    $('li').click(function (e){
+        e.preventDefault();
+        $('li').filter(':nth-child(1)').css('color', 'blue');
+    });
